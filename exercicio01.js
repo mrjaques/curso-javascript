5b44cc3227343792b37c029446701de5514ed17f
Gabriel Jaques <mrjaques7@gmail.com>
2026-01-19 15:25:45 -0300

    feat: aula 01 - trabalhando com sequencia de escapes em javascript

--- exercicio01.js
+++ exercicio01.js
@@ -13,3 +13,8 @@ let saudacao = "hello";
 console.log(saudacao[saudacao.length - 1]); // "o"
 
 /* O resultado será a letra "o", que é o último caractere da string "hello" */
+
+let poema = "Rosas são vermelhas, \nvioletas são azuis, \njavascript é incrível, \ne você também.";
+console.log(poema);
+/* O resultado será o poema impresso em múltiplas linhas, graças ao uso do caractere de nova linha \n */
+
