8f6179da65013f775914d0e663c36c7abb1d964f
Gabriel Jaques <mrjaques7@gmail.com>
2026-01-19 15:17:50 -0300

    feat: aula 01 - trabalhando com notação de colchetes

--- exercicio01.js
+++ exercicio01.js
@@ -8,3 +8,8 @@ book["fat"] = false;
 console.log(book);
 
 /* O resultado será um objeto com as propriedades topic, author e fat, onde fat é false */
+
+let saudacao = "hello";
+console.log(saudacao[saudacao.length - 1]); // "o"
+
+/* O resultado será a letra "o", que é o último caractere da string "hello" */
