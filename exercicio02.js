c1551ce840875d742473954ce698dcef50538927
Gabriel Jaques <mrjaques7@gmail.com>
2026-01-21 16:37:13 -0300

    feat: aula 02 - Conhecendo o método push e reverse

---
+++ exercicio02.js
@@ -0,0 +1,13 @@
+var a = [];
+a.push("A");
+a.push("B");
+a.push("C");
+console.log(a);
+// O resultado será um array com os elementos "A", "B" e "C"
+
+a.reverse();
+console.log(a);
+// após a inversão, o array será ["C", "B", "A"]
+
+
+
