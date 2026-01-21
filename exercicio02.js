4d585014ffa0ea4a7fa320927a786ddc123676d0
Gabriel Jaques <mrjaques7@gmail.com>
2026-01-21 16:53:21 -0300

    feat: aula 02 - Javascript dentro do html com tag script

--- exercicio02.js
+++ exercicio02.js
@@ -9,5 +9,16 @@ a.reverse();
 console.log(a);
 // após a inversão, o array será ["C", "B", "A"]
 
+function fator(n){
+    var produto = 1;
+    while(n > 1){
+        produto *= n;
+        n -- ;
+    }
+    return produto;
+}
+console.log(fator(5)); // 120
+/* O resultado será 120, que é o fatorial de 5 (5 * 4 * 3 * 2 * 1) */
+
 
 
